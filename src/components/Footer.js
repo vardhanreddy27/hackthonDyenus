8d1205f3ed6b269063893b9a24cd6572419d6544
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-08-25 23:21:15 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,42 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <hr className='hr' />
+    < div className='container'>
+    <div className='row mt-4 lr'>
+    <div className="col-4"><h4 className='primarycolor'>ABOUT</h4></div>
+    <div className="col-4"><h4 className='primarycolor'>HELP</h4></div>
+    <div className="col-4"><h4 className='primarycolor'>DEVELOPERS</h4></div>
+    </div>
+    <div className='row mt-4 lr'>
+    <div className="col-4"><h5 className='thin'>Contact</h5></div>
+    <div className="col-4"><h5 className='thin'>Cusomer Suport</h5></div>
+    <div className="col-4"><h5 className='thin'>Github</h5></div>
+    </div>
+       <div className='row mt-4 lr'>
+    <div className="col-4"><h5 className='thin'>Brand</h5></div>
+    <div className="col-4"><h5 className='thin'>TroubleShotting</h5></div>
+    <div className="col-4"><h5 className='thin'>Documentation</h5></div>
+    </div>
+    <div className='row mt-4 lr'>
+    <div className="col-4"><h5 className='thin'>Blog</h5></div>
+    <div className="col-4"><h5 className='thin'>Guides</h5></div>
+    <div className="col-4"><h5 className='thin'>Bug Bounty</h5></div>
+    </div>
+    <div className='row mt-4 lr'>
+    <div className="col-4"><h5 className='thin'>Community</h5></div>
+    <div className="col-4"><h5 className='thin'></h5></div>
+    <div className="col-4"><h5 className='thin'>Audits</h5></div>
+    </div>
+    <div className='row mt-4 lr'>
+    <div className="col-4"><h5 className='thin'>Token</h5></div>
+    <div className="col-4"><h5 className='thin'></h5></div>
+    <div className="col-4"><h5 className='thin'>Careers</h5></div>
+    </div>
+
+    </div>
+    </>
   )
 }
 
