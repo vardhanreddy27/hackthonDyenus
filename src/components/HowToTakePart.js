7c9428d45ebf1f9e180505e80335c93bda67b1d4
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-08-26 00:05:37 +0530

    howtotake  updated

--- src/components/HowToTakePart.js
+++ src/components/HowToTakePart.js
@@ -1,12 +1,58 @@
 import React from 'react'
+import { BsPlusLg } from 'react-icons/bs';
 
 function HowToTakePart() {
   return (
 
-    <> <div className="fluid-container bgcolor p-5"> 
+    <> <div className="fluid-container p-5"> 
     <div className='row'><h2><span className="thin">How to 
 </span><span className='primarycolor thin'> Take Part?</span></h2>
-    </div></div>
+    </div>
+    <div className='row'>
+    <div className='col-5'></div>
+    <div className='col-2'>
+</div>
+    <div className='col-5'><div className="box1">
+  <div className="card-body p-5 blur">
+  <span className="primarycolor font"><h6>Activate your Profile</h6></span><p className='thin'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, justo vel dapibus hendrerit,</p>
+  <button className='btn4'>Connect Wallet <BsPlusLg className='xs xss' /></button>
+  </div>
+</div></div>
+    </div>
+    <div className='row'>
+    <div className='col-5'><div className="box1">
+  <div className="card-body p-5 blur">
+  <span className="primarycolor font"><h6>Stake in IFO pool
+</h6></span><p className='thin'>Nulla neque nulla, varius in sodales eget, rhoncus vel felis. Duis interdum, nulla ac pellentesque imperdiet, libero odio vehicula turpis, nec eleifend urna ex in tortor. Cras vitae augue risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque sit amet mauris gravida nulla facilisis volutpat. Curabitur in libero dolor. </p>
+<p>How is the IFO credit calculated?</p>  </div>
+</div></div>
+    <div className='col-2'></div>
+    <div className='col-5'></div>
+    </div>
+    <div className='row'>
+    <div className='col-5'>
+      </div>
+    <div className='col-2'></div>
+    <div className='col-5'>  <div className="box1">
+  <div className="card-body p-5 blur">
+  <span className="primarycolor font"><h6>Commit
+</h6></span><p className='thin'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, justo vel dapibus hendrerit, </p>
+</div>
+</div></div>
+    </div>
+    <div className='row'>
+    <div className='col-5'>
+    <div className="box1">
+  <div className="card-body p-5 blur">
+  <span className="primarycolor font"><h6>Claim your tokens and achievement
+
+</h6></span><p className='thin'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, justo vel dapibus hendrerit,</p>
+</div>
+</div>  </div>
+    <div className='col-2'></div>
+    <div className='col-5'> </div>
+    </div>
+    </div>
 </>
 
     )
